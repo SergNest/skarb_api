3073703a2e14ca8ae23fbe1c565bffbacf56ad67
Serhii Nesterenko <sy.nesterenko@gmail.com>
2024-04-13 22:47:42 +0300

    type add

--- main.py
+++ main.py
@@ -45,5 +45,22 @@ async def get_data_from_external_api(client_id: str, authorization: str = Header
             raise HTTPException(status_code=500, detail="Error connecting to external API")
 
 
+@app.get("/gettype/{search}")
+async def get_data_from_external_api(search: str, authorization: str = Header(None)):
+    central_base_api_url = f"http://{settings.ip_central}:{settings.port_central}/central/hs/model/gettype/{search}"
+    if authorization != f"Bearer {expected_token}":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(central_base_api_url)
+            response.raise_for_status()
+            return response.json()
+        except httpx.HTTPStatusError as e:
+            raise HTTPException(status_code=e.response.status_code, detail="External API returned error")
+        except httpx.RequestError:
+            raise HTTPException(status_code=500, detail="Error connecting to external API")
+
+
 if __name__ == '__main__':
     uvicorn.run(app="main:app", reload=True, port=55335)
