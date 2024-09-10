599f9d2ee9b0a7e516b921320618589daa392bba
Serhii Nesterenko <sy.nesterenko@gmail.com>
2024-09-10 11:19:01 +0300

    photo-uniq base 64

--- photo.py
+++ photo.py
@@ -2,7 +2,7 @@ from PIL import Image
 import cv2
 import numpy as np
 from io import BytesIO
-
+import base64
 
 # Функція для порівняння зображень на основі ORB
 def compare_images(img1, img2):
@@ -21,3 +21,15 @@ def read_imagefile(file) -> np.array:
     image = Image.open(BytesIO(file))
     image = np.array(image.convert('RGB'))  # Конвертуємо у формат RGB
     return cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+
+
+# Функція для декодування base64 у зображення
+def decode_base64_image(image_base64: str):
+    image_data = base64.b64decode(image_base64)
+    # Перетворюємо байти у numpy array
+    np_arr = np.frombuffer(image_data, np.uint8)
+    # Читаємо зображення з numpy array
+    img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    if img is None:
+        raise ValueError("Error decoding image")
+    return img
--- work_with_foto/photo_route.py
+++ work_with_foto/photo_route.py
@@ -1,23 +1,41 @@
 from fastapi import APIRouter, UploadFile, HTTPException
-from photo import read_imagefile, compare_images
+from photo import read_imagefile, compare_images, decode_base64_image
+from work_with_foto.schema import ImageData
+
 
 router = APIRouter(
     prefix='/photo',
     tags=['work with photo']
 )
 
-
 @router.post("/unique_images")
-async def filter_unique_images(files: list[UploadFile]):
+async def filter_unique_images(files: list[ImageData]):
     try:
-        images = [(file.filename, read_imagefile(await file.read())) for file in files]
+        # Декодуємо зображення з base64 і зберігаємо їх разом з іменами
+        images = [(file.filename, decode_base64_image(file.image_base64)) for file in files]
         unique_images = []
 
         for filename, img in images:
+            # Перевіряємо, чи зображення унікальне
             if not any(compare_images(img, comp_img) for _, comp_img in unique_images):
                 unique_images.append((filename, img))
 
-        return len(unique_images)
+        return {"unique_count": len(unique_images), "unique_filenames": [filename for filename, _ in unique_images]}
 
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Server error: {str(e)}")
+
+# @router.post("/unique_images")
+# async def filter_unique_images(files: list[UploadFile]):
+#     try:
+#         images = [(file.filename, read_imagefile(await file.read())) for file in files]
+#         unique_images = []
+#
+#         for filename, img in images:
+#             if not any(compare_images(img, comp_img) for _, comp_img in unique_images):
+#                 unique_images.append((filename, img))
+#
+#         return len(unique_images)
+#
+#     except Exception as e:
+#         raise HTTPException(status_code=500, detail=f"Server error: {str(e)}")
