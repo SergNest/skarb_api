6bf99703bf14c373d8bfc4895cd13e5c17a2f593
Serhii Nesterenko <sy.nesterenko@gmail.com>
2024-11-21 12:22:19 +0200

    sf date

--- sun_flower/schema.py
+++ sun_flower/schema.py
@@ -1,12 +1,22 @@
 from pydantic import BaseModel, Field
-from typing import List, Optional
+from typing import Optional
+from datetime import datetime
 
 
 class DelayRequest(BaseModel):
-    Client_uuid: str = Field(..., description="Uuid of the client")
-    Comment: str = Field(..., description="Additional information about the delay")
-    Manager: Optional[str] = Field(None, description="Manager responsible for the request")
-    Pawnshop: Optional[str] = Field(None, description="Identifier of the pawnshop")
+    client_uuid: str = Field(..., description="Uuid of the client")
+    comment: str = Field(..., description="Additional information about the delay")
+    delay_date: datetime = Field(..., description="The date and time of the delay in format 'YYYY-MM-DD HH:MM:SS'")
+    manager: Optional[str] = Field(None, description="Manager responsible for the request")
+    pawnshop: Optional[str] = Field(None, description="Identifier of the pawnshop")
+
+
+    class Config:
+        anystr_strip_whitespace = True
+        json_encoders = {
+            datetime: lambda v: v.strftime('%Y-%m-%d %H:%M:%S')
+        }
+
 
 class SuccessResponse(BaseModel):
     status: str = Field(..., description="Status of the request")
