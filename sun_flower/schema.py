fba3cdfff781b10a3c80ed139583fb6fced28529
Serhii Nesterenko <sy.nesterenko@gmail.com>
2024-11-21 14:05:20 +0200

    date del

--- sun_flower/schema.py
+++ sun_flower/schema.py
@@ -6,16 +6,16 @@ from datetime import datetime
 class DelayRequest(BaseModel):
     client_uuid: str = Field(..., description="Uuid of the client")
     comment: str = Field(..., description="Additional information about the delay")
-    delay_date: datetime = Field(..., description="The date and time of the delay in format 'YYYY-MM-DD HH:MM:SS'")
+    # delay_date: datetime = Field(..., description="The date and time of the delay in format 'YYYY-MM-DD HH:MM:SS'")
     manager: Optional[str] = Field(None, description="Manager responsible for the request")
     pawnshop: Optional[str] = Field(None, description="Identifier of the pawnshop")
 
 
-    class Config:
-        anystr_strip_whitespace = True
-        json_encoders = {
-            datetime: lambda v: v.strftime('%Y-%m-%d %H:%M:%S')
-        }
+    # class Config:
+    #     anystr_strip_whitespace = True
+    #     json_encoders = {
+    #         datetime: lambda v: v.strftime('%Y-%m-%d %H:%M:%S')
+    #     }
 
 
 class SuccessResponse(BaseModel):
