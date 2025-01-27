30a919c7f137aa122c414bcdda9de21cf882c0c6
SergNest <sy.nesterenko@gmail.com>
2025-01-27 23:33:59 +0200

    serialize

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -2,6 +2,7 @@ from loguru import logger
 import requests
 import json
 import datetime
+import time 
 
 loki_url = "http://192.168.11.5:3100/loki/api/v1/push"
 headers = {"Content-Type": "application/json"}
