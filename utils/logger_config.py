3c8f42355e3eb83b8c9684279886cf417ba4af3b
Serhii Nesterenko <sy.nesterenko@gmail.com>
2025-01-27 11:24:50 +0200

    logs update

--- utils/logger_config.py
+++ utils/logger_config.py
@@ -10,6 +10,7 @@ headers = {
     "Content-Type": "application/json"
 }
 
+
 async def send_to_loki(message):
     try:
         record = json.loads(message)
@@ -22,7 +23,7 @@ async def send_to_loki(message):
         log_data = {
             "streams": [
                 {
-                    "stream": {"job": record["extra"].get("job", "skarbapi"), "level": level_name},
+                    "stream": {"job": record.get("job", "skarbapi"), "level": level_name},
                     "values": [
                         [
                             str(int(record["time"].timestamp() * 1000000000)),
@@ -54,45 +55,18 @@ async def send_to_loki(message):
 # Загальний лог-файл для відправки в Loki
 logger.add(send_to_loki, level="INFO")
 
+
 # Логи для окремих маршрутів, також відправляються в Loki
-logger.add(send_to_loki, level="INFO", filter=lambda record: "check_zok_by_phone" in record["extra"], extra={"job": "check_zok_by_phone"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "new_offer" in record["extra"], extra={"job": "new_offer"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "new_delay" in record["extra"], extra={"job": "new_delay"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "get_vendor" in record["extra"], extra={"job": "get_vendor"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "new_type" in record["extra"], extra={"job": "new_type"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "dogovorhistory" in record["extra"], extra={"job": "dogovorhistory"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "gettype" in record["extra"], extra={"job": "gettype"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "new_vendor" in record["extra"], extra={"job": "new_vendor"})
-logger.add(send_to_loki, level="INFO", filter=lambda record: "get_bonus_withdraw" in record["extra"], extra={"job": "get_bonus_withdraw"})
+def log_with_job(job_name):
+    return lambda record: logger.bind(job=job_name).log("INFO", record["message"])
 
-# from loguru import logger
-# import os
 
-# log_dir = "/var/log/fastapi"
-# os.makedirs(log_dir, exist_ok=True)
-
-# logger.remove()
-
-# # Загальний лог-файл
-# logger.add(f"{log_dir}/general.json", format="{time} {level} {message} {extra}", serialize=True, rotation="10 MB",
-#            retention="10 days", compression="zip")
-
-# # Логи для окремих маршрутів
-# logger.add(f"{log_dir}/check_zok_by_phone.json", serialize=True,
-#            filter=lambda record: "check_zok_by_phone" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/new_delay.json", serialize=True,
-#            filter=lambda record: "new_delay" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/get_vendor.json", serialize=True,
-#            filter=lambda record: "get_vendor" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/new_type.json", serialize=True,
-#            filter=lambda record: "new_type" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/dogovorhistory.json", serialize=True,
-#            filter=lambda record: "dogovorhistory" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/gettype.json", serialize=True,
-#            filter=lambda record: "gettype" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/new_vendor.json", serialize=True,
-#            filter=lambda record: "new_vendor" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/new_offer.json", serialize=True,
-#            filter=lambda record: "new_offer" in record["extra"], rotation="5 MB", retention="7 days")
-# logger.add(f"{log_dir}/get_bonus_withdraw.json", serialize=True,
-#            filter=lambda record: "get_bonus_withdraw" in record["extra"], rotation="5 MB", retention="7 days")
+logger.add(send_to_loki, level="INFO", filter=log_with_job("check_zok_by_phone"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("new_offer"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("new_delay"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("get_vendor"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("new_type"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("dogovorhistory"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("gettype"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("new_vendor"))
+logger.add(send_to_loki, level="INFO", filter=log_with_job("get_bonus_withdraw"))
